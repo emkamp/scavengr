f876a94a0f3c2476cd2a1f5626271e5a020af7ee
Emily Pulskamp <emily@empul.se>
2019-10-21 19:00:13 -0500

    date handling & datatables successfully wrangled

---
+++ routes/index.js
@@ -0,0 +1,91 @@
+// 'npm start' to start server at http://127.0.0.1:3000/
+
+var express = require('express');
+var router = express.Router();
+
+//const $ = require('jquery');
+const moment = require('moment');
+const https = require('https');
+
+var areaCount = '[area count]';
+var dataParsed = {};
+var dateArr = [];
+var today = moment().toISOString();
+console.log("today is " + today);
+
+function sortableDate(date) {
+  moment(date).toISOString();
+  return date;
+}
+
+https.get('https://data.austintexas.gov/resource/ffwg-tmw3.json', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    dataParsed = JSON.parse(data);
+    areaCount = Object.keys(dataParsed).length;
+    //console.log('areaCount = ' + areaCount);
+
+    for (var i = 0; i < areaCount; i++) {
+      var thisRoute = dataParsed[i].route_labl;
+
+      var nextDate = dataParsed[i].date_jan_jn;
+      var nextNextDate = dataParsed[i].date_jul_dc;
+      var nextDatePretty = '';
+      var nextDateSortable = sortableDate(nextDate);
+      var nextDateClass = '';
+
+      var next = [
+        nextDate,
+        nextNextDate
+      ].map(function(s){
+        return moment(s);
+      })
+      .sort(function(m){
+        return m.valueOf();
+      })
+      .find(function(m){return m.isAfter();});
+      
+      if (next) {
+        nextDatePretty = next.format("ll");
+      }
+      else {
+        nextDatePretty = "No more this year!";
+        nextDateClass = "past";
+      }
+
+      console.log(nextDatePretty);
+      let obj = {
+        route: thisRoute,
+        nextdate: nextDatePretty,
+        nextdatesortable: nextDateSortable,
+        nextdateclass: nextDateClass,
+      };
+
+      dateArr.push(obj);
+      //console.log(obj);
+    }
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+/*-------------------------------------------------------------*/
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Scavengr',
+    areacount: areaCount,
+    dataparsed: dataParsed,
+    datearr: dateArr
+  });
+});
+
+module.exports = router;
